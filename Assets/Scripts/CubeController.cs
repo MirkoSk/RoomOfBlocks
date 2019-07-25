58bb2b1d7f9b21a0ec399978f09a0e069b20355f
Mirko <mirkoskroch@gmail.com>
2019-07-25 23:00:34 +0200

    End of day 1

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    Vector3 origin = new Vector3();
+
+
+    void Awake()
+    {
+        origin = transform.localPosition;
+    }
+
+
+
+    public void MoveCube(float offset)
+    {
+        Vector3 newPosition = origin;
+        newPosition.y = origin.y + offset;
+        transform.localPosition = newPosition;
+    }
+}
---
+++ Assets/Scripts/CubeManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    [MinMaxRange(-1f, 1f)]
+    [SerializeField] RangedFloat offsetAmount = new RangedFloat();
+    CubeController[] cubes;
+
+
+
+    void Start()
+    {
+        cubes = GameObject.FindObjectsOfType<CubeController>();
+
+        MoveCubes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit")) MoveCubes();
+    }
+
+
+
+    void MoveCubes()
+    {
+        foreach (CubeController cube in cubes)
+        {
+            cube.MoveCube(Random.Range(offsetAmount.minValue, offsetAmount.maxValue));
+        }
+    }
+}
